ff177edd2151d87183a4591e39f72014a409a26e
Marlies Dens <marlies.dens@pxl.be>
2025-03-14 14:11:46 +0100

    oplossing

--- Talstelsel/MainWindow.xaml.cs
+++ Talstelsel/MainWindow.xaml.cs
@@ -20,10 +20,70 @@ namespace Talstelsel
         {
             InitializeComponent();
         }
+        private void inputTextBox_KeyDown(object sender, KeyEventArgs e)
+        {
+            // controleren of input hexadecimale waarde is of backspace
+        }
 
         private void convertButton_Click(object sender, RoutedEventArgs e)
         {
-            outputTextBox.Text = "yes";
+            string hex = inputTextBox.Text;
+
+            int dec = 0;
+
+            for (int i = 0, j = hex.Length -1 ; i < hex.Length; i++, j--)
+            {
+                int factor = (int)Math.Pow(16, j);
+
+                if (char.IsDigit(hex[i]))
+                {
+                    dec += (int)char.GetNumericValue(hex[i]) * factor;
+                }
+                else
+                {
+                    switch (hex[i])
+                    {
+                        case 'A':
+                        case 'a':
+                            dec += 10 * factor;
+                            break;
+                        case 'B':
+                        case 'b':
+                            dec += 11 * factor;
+                            break;
+                        case 'C':
+                        case 'c':
+                            dec += 12 * factor;
+                            break;
+                        case 'D':
+                        case 'd':
+                            dec += 13 * factor;
+                            break;
+                        case 'E':
+                        case 'e':
+                            dec += 14 * factor;
+                            break;
+                        case 'F':
+                        case 'f':
+                            dec += 15 * factor;
+                            break;
+                        default:
+                            MessageBox.Show("geef een correcte waarde in!", "waarschuwing");
+                            inputTextBox.Clear();
+                            inputTextBox.Focus();
+                            break;
+                    }
+                }
+              
+            }
+            outputTextBox.Text = $"{dec:N0}";
+
+            //int lengteInput = input.Length;
+            //string[] subs = input.Split();
+
+            // positie karakter bepaalt de macht. Cijfer x 16^(lengteInput-1) + cijfer x 16^(lengteInput--), ...
+            // doen zolang lengteInput >= 0
+
         }
 
         private void againButton_Click(object sender, RoutedEventArgs e)
@@ -37,5 +97,7 @@ namespace Talstelsel
         {
             Close();
         }
+
+       
     }
 }
