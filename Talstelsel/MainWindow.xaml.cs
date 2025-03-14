9526f001d0bbbac4775f85e419db48105356b688
Marlies Dens <marlies.dens@pxl.be>
2025-03-14 09:26:10 +0100

    knoppen

---
+++ Talstelsel/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Talstelsel
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void convertButton_Click(object sender, RoutedEventArgs e)
+        {
+            outputTextBox.Text = "yes";
+        }
+
+        private void againButton_Click(object sender, RoutedEventArgs e)
+        {
+            outputTextBox.Clear();
+            inputTextBox.Clear();
+            inputTextBox.Focus();
+        }
+
+        private void closeButton_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
